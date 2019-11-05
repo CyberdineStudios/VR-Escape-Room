731ddadf94ec28afd8a8ca3abec416d8c2f20fac
CyberdineStudios <45576180+CyberdineStudios@users.noreply.github.com>
2019-11-05 12:30:54 -0800

    Test Scripts Added

---
+++ VR-Cyberpunk-Escape-Room/Assets/Cyberpunk VR Escape Room/Scripts/Test Scripts/MenuButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    public RectTransform Recttransform;
+    public Image BackgroundImage;
+    public Image IconInage;
+    public RectTransform IconRecttransform;
+}
